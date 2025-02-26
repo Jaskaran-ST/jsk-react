9c2a3f9a64734ed910e9967318f3b69a8260fc17
Jaskaran-ST <jaskaran.singh02@st.com>
2025-02-26 19:46:28 +0530

    v4 hooks

---
+++ 03counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  // let counter = 5
+
+  // const addValue = () =>{
+  //   setCounter(counter+1)
+  //   console.log("value added")
+  // }
+
+  function addValue() {
+    if (counter < 20) {
+      setCounter(counter + 1)
+      console.log("value added")
+    }
+  }
+  function removeValue() {
+    if (counter > 0) {
+      setCounter(counter - 1)
+      console.log("value removed")
+    }
+  }
+
+  return (
+    <>
+      <h1>this is counter project</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
